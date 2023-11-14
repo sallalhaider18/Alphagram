1d95bc289c5aac9bb5a61c37ce17e965489c7431
sallalhaider <sallalhaider8@gmail.com>
2023-11-15 00:25:35 +0500

    complete folder structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 
 import './App.css';
 import {Routes ,Route } from 'react-router-dom';
-import signInForm from './_auth/forms/signInForm';
+import SignInForm from './_auth/Forms/SignInForm';
+import SignUpForm from './_auth/Forms/SignUpForm';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+import Home from './_root/Pages/Home';
+
 
 function App() {
   return (
     <Routes>
-      <Route path='/sign-in' element= {<signInForm />} />
-      <Route index element= {<Home />} />
 
+      { /* PUBLIC ROOTS */ }
+      <Route element = {<AuthLayout />}>
+         <Route path='/sign-in' element= {<SignInForm />} />
+         <Route path='/sign-up' element= {<SignUpForm />} />
+      </Route>
+
+      { /* PRIVATE ROOTS */ }
+      <Route element = {<RootLayout />}> 
+         <Route index element= {<Home />} />
+      </Route>
       
     </Routes>
     
